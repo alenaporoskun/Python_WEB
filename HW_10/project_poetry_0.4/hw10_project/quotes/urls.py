e050b25423a5f872fcc2cc75b27066d9b926036b
Olena Poroskun <56368022+alenaporoskun@users.noreply.github.com>
2024-03-11 19:21:22 +0200

    HW_10 vers_0.4

---
+++ HW_10/project_poetry_0.4/hw10_project/quotes/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "quotes"
+
+urlpatterns = [
+    path("", views.main, name="root"),
+    path("<int:page>", views.main, name="root_paginate"),
+    path('add/', views.add_quote, name='add_quote'),
+    path('author/<int:author_id>/', views.author_detail, name='author_detail')
+]
