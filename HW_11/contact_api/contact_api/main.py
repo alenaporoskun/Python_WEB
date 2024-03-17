3dac56711c86360a2377cadfacce06eff4075e0e
Olena Poroskun <56368022+alenaporoskun@users.noreply.github.com>
2024-03-17 17:58:21 +0200

    HW_10 vers_0.4 works

--- HW_11/contact_api/contact_api/main.py
+++ HW_11/contact_api/contact_api/main.py
@@ -4,7 +4,8 @@ from typing import List
 
 from database.database import engine, get_db
 from schemas.schemas import ContactCreate, Contact
-from crud import create_contact, get_contacts, get_contact, update_contact, delete_contact
+from crud import add_contact, get_contacts, get_contact, refresh_contact
+from crud import remove_contact, get_upcoming_birthdays
 from models.models import Base  
 
 app = FastAPI()
@@ -15,13 +16,20 @@ Base.metadata.create_all(bind=engine)
 # Create contact
 @app.post("/contacts/", response_model=Contact)
 def create_contact(contact: ContactCreate, db: Session = Depends(get_db)):
-    return create_contact(db=db, contact=contact)
+    return add_contact(db=db, contact=contact)
+
+# # Read contacts
+# @app.get("/contacts/", response_model=List[Contact])
+# def read_contacts(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+#     contacts = get_contacts(db=db, skip=skip, limit=limit)
+#     return contacts
 
 # Read contacts
 @app.get("/contacts/", response_model=List[Contact])
-def read_contacts(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    contacts = get_contacts(db=db, skip=skip, limit=limit)
-    return contacts
+def read_contacts(
+    skip: int = 0, limit: int = 10, query: str = None, db: Session = Depends(get_db)
+):
+    return get_contacts(db=db, skip=skip, limit=limit, query=query)
 
 # Read contact by ID
 @app.get("/contacts/{contact_id}", response_model=Contact)
@@ -37,7 +45,7 @@ def update_contact(contact_id: int, contact: ContactCreate, db: Session = Depend
     db_contact = get_contact(db=db, contact_id=contact_id)
     if db_contact is None:
         raise HTTPException(status_code=404, detail="Contact not found")
-    return update_contact(db=db, contact_id=contact_id, contact=contact)
+    return refresh_contact(db=db, contact_id=contact_id, contact=contact)
 
 # Delete contact
 @app.delete("/contacts/{contact_id}", response_model=Contact)
@@ -45,4 +53,9 @@ def delete_contact(contact_id: int, db: Session = Depends(get_db)):
     db_contact = get_contact(db=db, contact_id=contact_id)
     if db_contact is None:
         raise HTTPException(status_code=404, detail="Contact not found")
-    return delete_contact(db=db, contact_id=contact_id)
+    return remove_contact(db=db, contact_id=contact_id)
+
+# Get contacts with upcoming birthdays
+@app.get("/contacts/upcoming_birthdays/", response_model=List[Contact])
+def get_upcoming_birthdays_list(db: Session = Depends(get_db)):
+    return get_upcoming_birthdays(db=db)
